4fbadd3eff0d515cb948780ff3316e7a58e60892
lkhleekevin <33743221+lkhleekevin@users.noreply.github.com>
2017-11-19 15:22:10 +0800

    uyvyukb

--- node.h
+++ node.h
@@ -1,2 +1,13 @@
-
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
+#include "string.h"
 using namespace std;
+
+
+class Node {
+public:
+	string title, surname, givenName, dateOfBirth, address, phoneNumber, emailAddress, occupation, company;
+	int surnameID, givenNameID, ID;
+};
+#endif
